fa4a54a780113816fc6a80f4ce66d21b9bb72987
dalam3393 <32398041+dalam3393@users.noreply.github.com>
2017-10-15 22:47:04 -0400

    third change

--- namespace.js
+++ namespace.js
@@ -2,11 +2,12 @@ var DALA0016 = {
     init: function(){
         let div_box = document.createElement("div");
         div_box.classList.add("box");
-        divBox.textContent = "dala0016";
-        document.getElementById("boxes").appendChild(div_boxox);
-        box.addEventListener("click", DALA0016.mClick);
-        box.addEventListener("mouseover", DALA0016.mOver);
-        box.addEventListener("mouseout", DALA0016.mOut);
+        div_box.textContent = "dala0016";
+        let div_boxes = document.querySelector("#boxes");
+        div_box.addEventListener("click", DALA0016.mClick);
+        div_box.addEventListener("mouseover", DALA0016.mOver);
+        div_box.addEventListener("mouseout", DALA0016.mOut);
+        div_boxes.appendChild(div_box);
     },
     mClick: function(ev){
         ev.currentTarget.style.color = "#CCB200";
