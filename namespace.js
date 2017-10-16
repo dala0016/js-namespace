4f82b00b34e17ff84d9075cf675a4b6b1359376f
dalam3393 <32398041+dalam3393@users.noreply.github.com>
2017-10-15 22:40:00 -0400

    second change

--- namespace.js
+++ namespace.js
@@ -1,16 +1,16 @@
 var DALA0016 = { 
     init: function(){
-        let box = document.createElement("dala0016");
-        box.classList.add("boxes");
-        console.log("script in DALA0016 called");
-        box.addEventListener("click", this.mClick);
-        box.addEventListener("mouseover", this.mOver);
-        box.addEventListener("mouseout", this.mOut);
+        let div_box = document.createElement("div");
+        div_box.classList.add("box");
+        divBox.textContent = "dala0016";
+        document.getElementById("boxes").appendChild(div_boxox);
+        box.addEventListener("click", DALA0016.mClick);
+        box.addEventListener("mouseover", DALA0016.mOver);
+        box.addEventListener("mouseout", DALA0016.mOut);
     },
-    mClick: function (ev){
-        let target = ev.currentTarget;
-        target.style.color = #CCB200;
-        target.style.backgroundColor = #FFF9D4;
+    mClick: function(ev){
+        ev.currentTarget.style.color = "#CCB200";
+        ev.currentTarget.style.backgroundColor = "#FFF9D4";
     },
     mOver: function(ev){
         ev.currentTarget.classList.toggle("highlight");
@@ -18,26 +18,3 @@ var DALA0016 = {
     mOut: function (ev){
     ev.currentTarget.classList.toggle("highlight");
     } };
-
-
-    //<div id="boxes" class="box">dala0016</div>
-/*
-function init(ev){
-    let div = document.querySelector("#theDiv");
-    div.addEventListener("click", this.mClick);
-    div.addEventListener("mouseover", this.mOver);
-    div.addEventListener("mouseout", this.mOut);   
-}
-
-function mClick(ev){
-    //box.style.color = #CCB200;
-    DALA0016.style.color = #CCB200;
-    DALA0016.style.backgroundColor = #FFF9D4;
-}
-function mOver(ev){
-    ev.currentTarget.classList.toggle("highlight");
-}
-function mOut(ev){
-    ev.currentTarget.classList.toggle("highlight");
-}
-*/
